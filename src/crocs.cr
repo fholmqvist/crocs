391b5199ff1a1cb0343e82ebb9e8d8c466851aeb
holmqvist <fredrik.d.holmqvist@gmail.com>
2023-03-28 08:32:12 +0200

    feature: list all instance methods

--- src/crocs.cr
+++ src/crocs.cr
@@ -1,6 +1,30 @@
-# TODO: Write documentation for `Crocs`
-module Crocs
-  VERSION = "0.1.0"
+require "http/client"
+require "lexbor"
 
-  # TODO: Put your code here
+# "Hash", "String", "Time"
+
+{"Enumerable"}.each do |entry|
+  response = HTTP::Client.get "https://crystal-lang.org/api/1.7.3/#{entry}.html"
+  instance_methods = response.body.lines
+    .skip_while { |line|
+      line.includes? "Instance Method Detail"
+    }
+    .take_while { |line|
+      !line.includes? "View source"
+    }
+
+  parser = Lexbor::Parser.new(instance_methods.join("\n"))
+
+  entries = parser.css(".entry-summary").to_a
+
+  methods = entries.map { |entry|
+    entry.inner_text
+      .split("\n")
+      .map { |line| line.strip }
+      .reject { |line| line == "" }
+  }
+
+  methods.each { |method|
+    puts method
+  }
 end
