a263302a5b872166bd0d2b6a21fe622b7265b037
Holmqvist1990 <fredrik.d.holmqvist@gmail.com>
2023-03-28 19:24:48 +0200

    fix: only run find if both options are supplied

--- src/main.cr
+++ src/main.cr
@@ -55,18 +55,18 @@ OptionParser.parse do |parser|
 end
 
 if namespace.size > 0 && method.size == 0
-  STDERR.puts "ERROR: Searching requires both a namespace and a method to be searched for."
-  STDERR.puts "\n#{root_parser}"
+  STDERR.puts "ERROR: Searching requires both a namespace and a method to be searched for.\n\n"
+  STDERR.puts root_parser
   exit(1)
 end
 
 if namespace.size == 0 && method.size > 0
-  STDERR.puts "ERROR: Searching requires both a namespace and a method to be searched for."
-  STDERR.puts "\n#{root_parser}"
+  STDERR.puts "ERROR: Searching requires both a namespace and a method to be searched for.\n\n"
+  STDERR.puts root_parser
   exit(1)
 end
 
-if namespace && method
+if namespace.size > 0 && method.size > 0
   results, errors = docs.find(namespace, method)
 
   if errors.size > 0
@@ -76,7 +76,9 @@ if namespace && method
 
     STDERR.puts "\n#{root_parser}"
   else
-    puts "\n"
     print_results(results)
   end
+  puts ""
+else
+  puts root_parser
 end
